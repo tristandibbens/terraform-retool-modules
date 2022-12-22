6786307b3489fc87b99a2e0b393408d8d05fbdd9
tristan <29274865+tristandibbens@users.noreply.github.com>
2022-12-22 21:45:33 +0000

    reverted frickin rds

--- modules/aws_ecs_ec2/security.tf
+++ modules/aws_ecs_ec2/security.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "rds" {
     from_port   = "5432"
     to_port     = "5432"
     protocol    = "tcp"
-    security_groups = aws_security_group.ec2.id
+    security_groups = [aws_security_group.ec2.id]
   }
 
   # Allow all outbound - modify if necessary
