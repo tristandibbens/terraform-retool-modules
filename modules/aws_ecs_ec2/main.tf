6c8268fcc4c84da4f2505116d11a912629dc2f92
tristan <29274865+tristandibbens@users.noreply.github.com>
2022-12-22 20:23:59 +0000

    Removed provider reference allowing me to pass in a provider from my project

--- modules/aws_ecs_ec2/main.tf
+++ modules/aws_ecs_ec2/main.tf
@@ -148,7 +148,7 @@ resource "aws_db_instance" "this" {
   instance_class               = var.rds_instance_class
   engine                       = "postgres"
   engine_version               = "10.6"
-  name                         = "hammerhead_production"
+  db_name                         = "hammerhead_production"
   username                     = aws_secretsmanager_secret_version.rds_username.secret_string
   password                     = aws_secretsmanager_secret_version.rds_password.secret_string
   port                         = 5432
