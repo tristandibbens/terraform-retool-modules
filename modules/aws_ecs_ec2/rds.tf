c78dd15624a27a90c1939281f6525f22075660fc
tristan <29274865+tristandibbens@users.noreply.github.com>
2022-12-22 20:53:11 +0000

    Updated rds to aurora plus some fine tuning

--- modules/aws_ecs_ec2/rds.tf
+++ modules/aws_ecs_ec2/rds.tf
@@ -1,20 +1,15 @@
 resource "aws_rds_cluster" "this" {
   cluster_identifier    = "${var.deployment_name}-rds-instance"
-  db_name               = "hammerhead_production"
-  username              = aws_secretsmanager_secret_version.rds_username.secret_string
-  password              = aws_secretsmanager_secret_version.rds_password.secret_string
+  database_name         = "hammerhead_production"
+  master_username       = aws_secretsmanager_secret_version.rds_username.secret_string
+  master_password       = aws_secretsmanager_secret_version.rds_password.secret_string
   engine                = "aurora-postgresql"
   engine_verions        = "13.6"
   engine_mode           = "serverless"
-  replica_scale_enabled = false
-  replica_count         = 0
-  subnets             = var.subnet_ids
   vpc_id              = var.vpc_id
   vpc_security_group_ids       = [aws_security_group.rds.id]
-  monitoring_interval = 60
   skip_final_snapshot = true
   storage_encrypted   = true
-  publicly_accessible          = var.rds_publicly_accessible
 
   scaling_configuration = {
     auto_pause               = true
