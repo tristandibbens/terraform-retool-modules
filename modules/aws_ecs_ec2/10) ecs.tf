1c9ead2b41fdf5df0588ab36f7c6a6f3487b85f1
tristan <29274865+tristandibbens@users.noreply.github.com>
2022-12-27 13:47:20 +0000

    Changed namining conventions of .tf files, moved main.tf into servicefiles

--- modules/aws_ecs_ec2/10) ecs.tf
+++ modules/aws_ecs_ec2/10) ecs.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
-  }
-}
-
-# provider "aws" {
-#   region = var.aws_region
-# }
-
 resource "aws_ecs_cluster" "this" {
   name = "${var.deployment_name}-ecs"
 
@@ -140,30 +128,6 @@ resource "aws_ecs_capacity_provider" "this" {
 resource "aws_cloudwatch_log_group" "this" {
   name              = "${var.deployment_name}-ecs-log-group"
   retention_in_days = var.log_retention_in_days
-}
-
-resource "aws_db_subnet_group" "db_subnet_sg" {
-  name       = "${var.deployment_name}-db_subnet_sg"
-  subnet_ids = var.subnet_ids
-}
-
-resource "aws_db_instance" "this" {
-  identifier                   = "${var.deployment_name}-rds-instance"
-  allocated_storage            = 80
-  instance_class               = var.rds_instance_class
-  engine                       = "postgres"
-  engine_version               = "13.7"
-  db_name                      = "hammerhead_production"
-  username                     = aws_secretsmanager_secret_version.rds_username.secret_string
-  password                     = aws_secretsmanager_secret_version.rds_password.secret_string
-  port                         = 5432
-  publicly_accessible          = var.rds_publicly_accessible
-  db_subnet_group_name         = aws_db_subnet_group.db_subnet_sg.name
-  vpc_security_group_ids       = [aws_security_group.rds.id]
-  performance_insights_enabled = var.rds_performance_insights_enabled
-  
-  skip_final_snapshot          = true
-  apply_immediately            = true
 }
 
 resource "aws_ecs_service" "retool" {
