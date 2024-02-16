0365c3ad734e79d00629bff1c29703cea8fdb0ce
Tristan <29274865+tristandibbens@users.noreply.github.com>
2024-02-16 16:55:53 +0000

    Updated EC2 security group rule dependency to address drift.

--- modules/aws_ecs_ec2/15) security.tf
+++ modules/aws_ecs_ec2/15) security.tf
@@ -63,17 +63,14 @@ resource "aws_security_group" "ec2" {
   description = "Retool EC2 security group"
   vpc_id      = var.vpc_id
 
-  dynamic "ingress" {
-    for_each = var.ec2_ingress_rules
-    content {
-      description      = ingress.value["description"]
-      from_port        = ingress.value["from_port"]
-      to_port          = ingress.value["to_port"]
-      protocol         = ingress.value["protocol"]
-      cidr_blocks      = ingress.value["cidr_blocks"]
-      ipv6_cidr_blocks = ingress.value["ipv6_cidr_blocks"]
-    }
+  ingress {
+    description = "alb inbound only"
+    from_port   = "0"
+    to_port     = " 65535"
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb.id]
   }
+
 
   dynamic "egress" {
     for_each = var.ec2_egress_rules
